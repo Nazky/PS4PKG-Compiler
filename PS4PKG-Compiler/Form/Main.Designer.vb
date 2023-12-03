2b42b8ecddcedba2bdcdd77045637cb7793c4fc2
Nazky <56560636+Nazky@users.noreply.github.com>
2023-12-03 17:38:53 +0100

    First push

---
+++ PS4PKG-Compiler/Form/Main.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.Location = New System.Drawing.Point(60, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(423, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Drag and drop"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Folder :"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(471, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Compile PKG"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 67)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(471, 105)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(432, 177)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(51, 13)
+        Me.LinkLabel1.TabIndex = 4
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "NazkyYT"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(495, 199)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PS4PKG-Compiler"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ PS4PKG-Compiler/Form/Main.vb
@@ -0,0 +1,106 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Threading
+
+Public Class Main
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If Directory.Exists("tools") Then
+            Else
+                File.WriteAllBytes("tools.zip", My.Resources.tools)
+                ZipFile.ExtractToDirectory("tools.zip", "tools")
+                File.Delete("tools.zip")
+            End If
+            If TextBox1.Text = "" Or TextBox1.Text = "Drag and drop" Then
+                MsgBox("Please put a valid path.", MsgBoxStyle.Critical)
+            ElseIf Directory.Exists(TextBox1.Text) Then
+                Dim spkg As New SaveFileDialog
+                spkg.Title = "Choose where you want to save you'r package."
+                spkg.FileName = Path.GetFileNameWithoutExtension(TextBox1.Text)
+                spkg.Filter = "PS4/PS5 Package (*.pkg)|*.pkg"
+                spkg.AddExtension = True
+
+                If spkg.ShowDialog = DialogResult.OK Then
+                    cpkg(spkg.FileName, TextBox1.Text)
+                    'MsgBox(spkg.FileName)
+                End If
+            Else
+                MsgBox("Folder not found.", MsgBoxStyle.Critical)
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Sub cpkg(pkgn As String, ppkg As String)
+        Try
+            gen(ppkg)
+            cid(ppkg)
+            syscmd("tools\orbis-pub-cmd.exe", "img_create " & Chr(34) & ppkg & ".gp4" & Chr(34) & " " & Chr(34) & pkgn & Chr(34), Me)
+            File.Delete(ppkg & ".gp4")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Sub gen(ppkg As String)
+        Try
+            syscmd("tools\gengp4.exe", """" & ppkg & """", Me)
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Sub cid(ppkg As String)
+        Try
+            Dim gp4 As String = System.IO.File.ReadAllText(ppkg & ".gp4")
+            System.IO.File.WriteAllText(ppkg & ".gp4", gp4.Replace("<scenarios default_id=""1"">", "<scenarios default_id=""0"">"))
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Shared Sub syscmd(fexec As String, farg As String, frm As Main)
+        Dim oProcess As New Process()
+        Dim oStartInfo As New ProcessStartInfo(fexec, farg)
+        oStartInfo.UseShellExecute = False
+        oStartInfo.RedirectStandardOutput = True
+        oStartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        oStartInfo.CreateNoWindow = True
+        oStartInfo.RedirectStandardError = True
+        oProcess.StartInfo = oStartInfo
+        oProcess.Start()
+        oProcess.WaitForExit()
+
+        Using oStreamReader As System.IO.StreamReader = oProcess.StandardOutput
+            frm.Invoke(Sub() frm.RichTextBox1.Text = frm.RichTextBox1.Text & oStreamReader.ReadToEnd())
+        End Using
+        'Console.WriteLine(sOutput)
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+        RichTextBox1.SelectionStart = RichTextBox1.Text.Length
+        RichTextBox1.ScrollToCaret()
+    End Sub
+
+    Private Sub TextBox1_DragEnter(sender As Object, e As DragEventArgs) Handles TextBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub TextBox1_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox1.DragDrop
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+        For Each path In files
+            TextBox1.Text = path
+        Next
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://twitter.com/NazkyYT")
+    End Sub
+End Class
